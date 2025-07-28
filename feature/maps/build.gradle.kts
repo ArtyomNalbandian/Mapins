f13b766ec0b5d5923ff905be1802eec0d4ce1e21
bain tsyrempilov <batsbad@mail.ru>
2025-07-28 21:04:30 +0300

    maps lib dependency

--- feature/maps/build.gradle.kts
+++ feature/maps/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.maps.mobile)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
