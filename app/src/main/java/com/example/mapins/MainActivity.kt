766985d9de383b66efba02122232d8f1042a6de7
ArtyomNalbandian <artyomnalbandian11a@mail.ru>
2025-07-29 01:26:33 +0300

    setup: feature:maps update

--- app/src/main/java/com/example/mapins/MainActivity.kt
+++ app/src/main/java/com/example/mapins/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mapins.ui.theme.MapinsTheme
+import com.example.maps.YandexMapScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             MapinsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    YandexMapScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MapinsTheme {
-        Greeting("Android")
     }
 }
--- feature/maps/src/main/java/com/example/maps/MapScreen.kt
+++ feature/maps/src/main/java/com/example/maps/MapScreen.kt
@@ -1,7 +1,23 @@
 package com.example.maps
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
+
 @Composable
-fun YandexMapScreen() {
+fun YandexMapScreen(
+    modifier: Modifier = Modifier
+) {
+
     val context = LocalContext.current
     val mapView = remember {
         MapView(context).apply {
