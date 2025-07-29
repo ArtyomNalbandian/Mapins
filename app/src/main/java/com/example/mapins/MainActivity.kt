19ee16834ec4f0bfefc2e0a27ab7f01f002c931c
ArtyomNalbandian <artyomnalbandian11a@mail.ru>
2025-07-29 12:34:27 +0300

    app-state: start MainActivityViewModel logic

--- app/src/main/java/com/example/mapins/MainActivity.kt
+++ app/src/main/java/com/example/mapins/MainActivity.kt
@@ -9,14 +9,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mapins.ui.theme.MapinsTheme
+import com.example.mapins.util.isSystemInDarkTheme
 import com.example.maps.YandexMapScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val themeSettings by mutableStateOf(
+            ThemeSettings(
+                darkTheme = resources.configuration.isSystemInDarkTheme,
+                androidTheme = Loading
+            )
+        )
+
         enableEdgeToEdge()
         setContent {
             MapinsTheme {
@@ -33,3 +43,14 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+private val lightScrim = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+
+
+private val darkScrim = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b)
+
+data class ThemeSettings(
+    val darkTheme: Boolean,
+    val androidTheme: Boolean,
+    val disableDynamicTheming: Boolean,
+)
---
+++ app/src/main/java/com/example/mapins/MainActivityViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.mapins
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.StateFlow
+
+class MainActivityViewModel() : ViewModel() {
+    val uiState: StateFlow
+}
+
+sealed interface MainActivityUiState {
+    data object Loading : MainActivityUiState
+
+    data class Success(val userData: UserData) : MainActivityUiState {
+        override val shouldDisableDynamicTheming = !userData.useDynamicColor
+
+        override val shouldUseAndroidTheme: Boolean = when (userData.themeBrand) {
+            ThemeBrand.DEFAULT -> false
+            ThemeBrand.ANDROID -> true
+        }
+
+        override fun shouldUseDarkTheme(isSystemDarkTheme: Boolean) =
+            when (userData.darkThemeConfig) {
+                DarkThemeConfig.FOLLOW_SYSTEM -> isSystemDarkTheme
+                DarkThemeConfig.LIGHT -> false
+                DarkThemeConfig.DARK -> true
+            }
+    }
+
+    /**
+     * Returns `true` if the state wasn't loaded yet and it should keep showing the splash screen.
+     */
+    fun shouldKeepSplashScreen() = this is Loading
+
+    /**
+     * Returns `true` if the dynamic color is disabled.
+     */
+    val shouldDisableDynamicTheming: Boolean get() = true
+
+    /**
+     * Returns `true` if the Android theme should be used.
+     */
+    val shouldUseAndroidTheme: Boolean get() = false
+
+    /**
+     * Returns `true` if dark theme should be used.
+     */
+    fun shouldUseDarkTheme(isSystemDarkTheme: Boolean) = isSystemDarkTheme
+}
